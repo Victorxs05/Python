5e330020453609f94cfc2c44b6dbfcc369bffbdc
Victorxs05 <160060912+Victorxs05@users.noreply.github.com>
2024-05-02 11:29:38 -0300

    Update 1.5 Atividade - Numeros pares e impares.py

--- 2.Laço de repetição/1.5 Atividade - Numeros pares e impares.py
+++ 2.Laço de repetição/1.5 Atividade - Numeros pares e impares.py
@@ -10,10 +10,12 @@ for i in range(1, 6):
 for i in range(1, 6): 
     if ( i % 2 == 0):
         print(f"Numeros pares: {i}")
+        #pares += 1
 
 for i in range(1, 6): 
     if ( i % 2 != 0): 
         print(f"Numeros impares: {i}")
+        #impares += 1
 
 #print(f"Numeros pares: {pares}")
 #print(f"Numeros impares: {impares}")
