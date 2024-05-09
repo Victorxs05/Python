d9ef3c25c92c3b0888803a8571acbd550a106b08
Victorxs05 <160060912+Victorxs05@users.noreply.github.com>
2024-05-09 11:06:29 +0000

    Add 1

--- 2.Laço de repetição/2. Atividade - 4 notas e media.py
+++ 2.Laço de repetição/2. Atividade - 4 notas e media.py
@@ -1,4 +1,5 @@
 import os
+<<<<<<< HEAD
 import time
 os.system("clear")
 
@@ -16,3 +17,17 @@ while True:
 
 media = soma / contador
 print(f"Media: {media}")
+=======
+
+os.system("cls || clear")
+
+soma = 0 
+media = 0 
+
+for i in range(1, 5): 
+    numeros = int(input(f"Digite a {i}ª nota: "))
+    soma += numeros
+    media = soma / 4
+
+print(f"Media : {media}")
+>>>>>>> c891460 (Aula 4)
